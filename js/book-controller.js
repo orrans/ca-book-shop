9ef17f90e5ff6e2e386584d84b063809f7ed0fb1
orrans <orrans@gmail.com>
2025-10-31 14:15:05 +0200

    Add modal

--- js/book-controller.js
+++ js/book-controller.js
@@ -40,3 +40,34 @@ function onAddbook() {
     addBook(title, price)
     renderBooks()
 }
+
+function onReadBook(bookId) {
+    const elModal = document.querySelector('.modal')
+    const elModalContent = document.querySelector('.modal-inner-content')
+    elModal.classList.add('show')
+    const book = getBook(bookId)
+
+    const bookHtml = `
+    <img src="${book.imgUrl}"/>
+    <h3>${book.title}</h3>
+    <h4>$${book.price}</h4>
+
+    <div class="rating">
+        <button onclick="onUpdateRating('${bookId}',-1)">-</button>
+        <span class="book-rating">${book.rating}</span>
+        <button onclick="onUpdateRating('${bookId}',1)">+</button>
+    </div>
+    `
+    elModalContent.innerHTML = bookHtml
+}
+
+function onCloseModal() {
+    const elModal = document.querySelector('.modal')
+    elModal.classList.remove('show')
+}
+
+function onUpdateRating(bookId, amount) {
+    const bookRating = updateRating(bookId, amount)
+    const elBookRating = document.querySelector('.book-rating')
+    elBookRating.innerText = bookRating
+}
--- js/services/book-service.js
+++ js/services/book-service.js
@@ -3,24 +3,31 @@ let gBooks = [
         id: 'b101',
         title: 'The Adventures of Lori Ipsi',
         price: 120,
+        rating: 2,
         imgUrl: 'https://picsum.photos/200/300/?1',
     },
     {
         id: 'b102',
         title: 'The Hidden Kingdom',
         price: 90,
+        rating: 2,
         imgUrl: 'https://picsum.photos/200/300/?2',
     },
     {
         id: 'b103',
         title: 'Coding for Wizards',
         price: 200,
+        rating: 2,
         imgUrl: 'https://picsum.photos/200/300/?3',
     },
 ]
 
 function getBooks() {
     return gBooks
+}
+
+function getBook(bookId) {
+    return gBooks.find((book) => book.id === bookId)
 }
 
 function removeBook(bookId) {
@@ -45,12 +52,22 @@ function addBook(title, price) {
     console.log(book)
 }
 
+function updateRating(bookId, amount) {
+    const bookIdx = gBooks.findIndex((book) => book.id === bookId)
+    gBooks[bookIdx].rating += amount
+    const bookRating = gBooks[bookIdx].rating
+    if (bookRating < 0) gBooks[bookIdx].rating = 0
+    else if (bookRating > 5) gBooks[bookIdx].rating = 5
+    return gBooks[bookIdx].rating
+}
+
 function _createBook(title, price) {
     const randImgId = getRandomInt(1, 9999)
     return {
         id: `b${getRandomInt(100, 1000)}`,
         title,
         price,
+        rating: 0,
         imgUrl: `https://picsum.photos/200/300/?${randImgId}`,
     }
 }
